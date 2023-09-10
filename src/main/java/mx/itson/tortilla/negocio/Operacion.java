19e6f709c75168ebe07836170b6b262d29da05e7
José Alvarez <Jos Alvarez@LAPTOP-HUTCQMT9>
2023-09-10 00:54:37 -0700

    Versión inicial del proyecto tortilla.

---
+++ src/main/java/mx/itson/tortilla/negocio/Operacion.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mx.itson.tortilla.negocio;
+
+/**
+ *
+ * @author José Alvarez
+ */
+public class Operacion {
+
+
+public String[] separar (String oracion){
+      // Claro que si mi gallito
+      String[]resultado = oracion.split("-");
+              return resultado;
+  }
+  
+}
+
+
+
+    
+
